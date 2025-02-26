c9659c3b8455f548a71cbf75fd21400f39c5e0e9
Matheus Torres <30413727+matttheust@users.noreply.github.com>
2025-02-26 15:05:50 -0300

    feat: ajusta posição do menu hamburguer para o canto mais à esquerda

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import Logo from '../Logo/Logo';
-import Navbar from '../Navbar/Navbar';
-import UserGreeting from '../UserGreeting/UserGreeting';
 import './Header.css';
 
 const Header: React.FC = () => {
   return (
     <header className="header">
-      <div className="header-left">
-        <Logo />
-      </div>
-      <div className="header-center">
-        <Navbar />
-      </div>
+      {/* Menu Hamburguer */}
+      <button className="menu-button">
+        <span className="menu-icon"></span>
+      </button>
+
+      {/* Logo */}
+      <Logo />
+
+      {/* Seção Central – Navegação */}
+      <nav className="header-center">
+        <ul className="nav-links">
+          <li className="active">Clientes</li>
+          <li>Clientes selecionados</li>
+          <li>Sair</li>
+        </ul>
+      </nav>
+
+      {/* Seção Direita – Saudação do Usuário */}
       <div className="header-right">
-        <UserGreeting />
+        <span className="user-greeting">Olá, <strong>Usuário</strong>!</span>
       </div>
     </header>
   );
