94b0142fd1d852f6d30a650d3f307d315db31b1c
Matheus Torres <30413727+matttheust@users.noreply.github.com>
2025-02-26 14:29:39 -0300

    feat: configura imagem logo e posicionamento

--- frontend/src/components/Logo/Logo.tsx
+++ frontend/src/components/Logo/Logo.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import logo from '../../assets/logo.svg'; // Ajuste o caminho conforme necessário
 import './Logo.css';
 
 const Logo: React.FC = () => {
-  return <div className="logo">teddy CRM Finance</div>;
+  return (
+    <div className="logo">
+      <img src={logo} alt="Logo teddy CRM Finance" />
+    </div>
+  );
 };
 
 export default Logo;
